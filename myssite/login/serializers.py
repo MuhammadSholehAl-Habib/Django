75c432e465f8eca75c6c3a8a8cbdb00dc614e800
Muhammad Sholeh Al - Habib <m.sholehalhabibsan@gmail.com>
2018-07-17 15:08:37 +0700

    Choropleth dengan API (Final) + Show Information Control

--- myssite/login/serializers.py
+++ myssite/login/serializers.py
@@ -17,10 +17,10 @@ class organizationSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = organization
-        fields = ('id','url','name','level','priority_type','combat_type','is_induk','is_rahwan','parent_id','level_0_id','latitude','longitude','img_path','leader','vice_leader')
+        fields = ('id','url','name','level')
 
 class ekkoSerializer(serializers.HyperlinkedModelSerializer):
 
     class Meta:
         model = ekko
-        fields = ('id','url','personnel_quantity','personnel_quality','personnel_mantap','personnel_status','materiil_quantity','materiil_quality','materiil_mantap','materiil_status','exercise_quantity','exercise_quality','exercise_mantap','exercise_status','pangkalan_quantity','pangkalan_quality','pangkalan_mantap','pangkalan_status','penak_quantity','penak_quality','penak_mantap','penak_status','quantity','quality','mantap','status','induk','periode','year','satuan_id')
+        fields = ('id','url','mantap','satuan_id')
